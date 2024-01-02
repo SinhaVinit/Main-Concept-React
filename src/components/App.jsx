fd4f26943176200c7805dd1beb685dd238034389
SinhaVinit <vinit.8444935532@gmail.com>
2024-01-02 22:16:23 +0530

    Creating static and live clock. Rough

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,29 +1,12 @@
 import "./App.css";
-const users = [
-  {
-    id: 1,
-    name: "Vinit",
-  },
-  {
-    id: 2,
-    name: "Vishal",
-  },
-  {
-    id: 3,
-    name: "Aakash",
-  },
-  {
-    id: 4,
-    name: "Guest",
-  },
-];
-
-function formatUser(user) {
-  return <h1>Hello {user.name}</h1>;
-}
 
 function App() {
-  return <div className="App">{users.map(formatUser)}</div>;
+  // const date = new Date();
+  return (
+    <div className="App">
+      <h1>{new Date().toLocaleTimeString()}</h1>
+    </div>
+  );
 }
 
 export default App;
