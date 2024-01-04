dd51165b40b20a91c447d4dfb41db5cceca3ca17
SinhaVinit <vinit.8444935532@gmail.com>
2024-01-04 13:50:25 +0530

    Extracting Components 2

---
+++ src/components/UserInfo.jsx
@@ -0,0 +1,12 @@
+import Avatar from "./Avatar";
+
+function UserInfo(props) {
+  return (
+    <div>
+      <Avatar user={props.user} />
+      <div>{props.user.name}</div>
+    </div>
+  );
+}
+
+export default UserInfo;
--- src/components/comment.jsx
+++ src/components/comment.jsx
@@ -1,3 +1,5 @@
+import UserInfo from "./UserInfo";
+
 function formatDate(date) {
   return date.toLocaleDateString();
 }
@@ -5,14 +7,7 @@ function formatDate(date) {
 function Comment(props) {
   return (
     <div className="Comment">
-      <div className="UserInfo">
-        <img
-          className="Avatar"
-          src={props.author.avatarUrl}
-          alt={props.author.name}
-        />
-        <div className="UserInfo-name">{props.author.name}</div>
-      </div>
+      <UserInfo user={props.author} />
       <div className="Comment-text">{props.text}</div>
       <div className="Comment-date">{formatDate(props.date)}</div>
     </div>
