a3215dfd946d2b59fe1051c60d18c53cf80e8710
SinhaVinit <vinit.8444935532@gmail.com>
2024-01-04 09:53:34 +0530

    Components and Props 1 using ES6 class

--- src/components/welcome.jsx
+++ src/components/welcome.jsx
@@ -1,7 +1,14 @@
 import "./welcome.css";
+import React from "react";
 
-function Welcome(props) {
-  return <h1>Hello, {props.name}!</h1>;
+// function Welcome(props) {
+//   return <h1>Hello, {props.name}!</h1>;
+// }
+
+class Welcome extends React.Component {
+  render() {
+    return <h1>Hello, {this.props.name}</h1>;
+  }
 }
 
 export default Welcome;
