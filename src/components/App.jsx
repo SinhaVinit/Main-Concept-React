7fd655cd1c9884f4159a179c2dc2dd31546c1344
SinhaVinit <vinit.8444935532@gmail.com>
2024-01-04 12:27:37 +0530

    Components and Props 2 Rendering a Component in App.jsx file

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,9 +2,11 @@ import "./App.css";
 import Welcome from "./welcome";
 
 function App() {
+  const element = <Welcome name="Vishal Sinha" />; // React Element
   return (
     <div className="App">
       <Welcome name="Vinit Sinha" />
+      {element}
     </div>
   );
 }
--- src/components/welcome.jsx
+++ src/components/welcome.jsx
@@ -1,13 +1,17 @@
 import "./welcome.css";
 import React from "react";
 
+// Declaration of React component or function componen with properties
+
+//React Component
 // function Welcome(props) {
 //   return <h1>Hello, {props.name}!</h1>;
 // }
 
+//ES6 class
 class Welcome extends React.Component {
   render() {
-    return <h1>Hello, {this.props.name}</h1>;
+    return <h1>Hello, {this.props.name}!</h1>;
   }
 }
 
