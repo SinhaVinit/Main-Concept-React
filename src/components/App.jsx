0c51cb18091d3d0ffb84fe108d302f990f08d1cf
SinhaVinit <vinit.8444935532@gmail.com>
2024-01-10 13:19:50 +0530

    Conditional Rendering 2 (Element Variables)

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,19 +1,61 @@
 import "./App.css";
-
-function UserGreeting(props) {
-  return <h1>Welcome back!</h1>;
-}
-
-function GuestGreeting(props) {
-  return <h1>Please sign up!</h1>;
-}
+import React from "react";
 
 function Greeting(props) {
   const isLoggedIn = props.isLoggedIn;
   if (isLoggedIn) {
-    return <UserGreeting />;
+    return <h1>Welcome Back!</h1>;
   }
-  return <GuestGreeting />;
+  return <h1>Please Sign Up!</h1>;
 }
 
-export default Greeting;
+function LoginButton(props) {
+  return (
+    <button className="LoginButton" onClick={props.onClick}>
+      Login
+    </button>
+  );
+}
+
+function LogoutButton(props) {
+  return (
+    <button className="LogoutButton" onClick={props.onClick}>
+      Logout
+    </button>
+  );
+}
+
+class LoginControl extends React.Component {
+  constructor(props) {
+    super(props);
+    this.handelLoginClick = this.handelLoginClick.bind(this);
+    this.handelLogoutClick = this.handelLogoutClick.bind(this);
+    this.state = { isLoggedIn: true };
+  }
+
+  handelLoginClick() {
+    this.setState({ isLoggedIn: true });
+  }
+
+  handelLogoutClick() {
+    this.setState({ isLoggedIn: false });
+  }
+
+  render() {
+    const isLoggedIn = this.state.isLoggedIn;
+    let button;
+    if (isLoggedIn) {
+      button = <LogoutButton onClick={this.handelLogoutClick} />;
+    } else {
+      button = <LoginButton onClick={this.handelLoginClick} />;
+    }
+    return (
+      <div className="App">
+        <Greeting isLoggedIn={isLoggedIn} />
+        {button}
+      </div>
+    );
+  }
+}
+
+export default LoginControl;
--- src/index.js
+++ src/index.js
@@ -1,12 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
-import Greeting from "./components/App";
+import LoginControl from "./components/App";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <div>
-    {/* Try changing to isLoggedIn={false} */}
-    <Greeting isLoggedIn={true} />
+    <LoginControl />
   </div>
 );
