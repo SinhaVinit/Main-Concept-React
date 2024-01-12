3675a5d4319c07380a892097d966f58a9b6f462c
SinhaVinit <vinit.8444935532@gmail.com>
2024-01-12 18:37:14 +0530

    Conditional Rendering 4 (Inline If-Else with Conditional Operator)

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,60 @@
 import "./App.css";
 import React from "react";
 
-function MailBox(props) {
-  const unreadMessages = props.unreadMessages;
+function Message(props) {
   return (
-    <div className="App">
-      <h1>Hello Vinit!</h1>
-      {unreadMessages.length > 0 && (
-        <h2>You have {unreadMessages.length} unread messages.</h2>
-      )}
-    </div>
+    <h1>The user is {props.isLoggedIn ? "currently" : "not"} logged in.</h1>
   );
 }
 
-export default MailBox;
+function LoginButton(props) {
+  return (
+    <button onClick={props.onClick} className="LoginButton">
+      Login
+    </button>
+  );
+}
+
+function LogoutButton(props) {
+  return (
+    <button onClick={props.onClick} className="LogoutButton">
+      Logout
+    </button>
+  );
+}
+
+class Greeting extends React.Component {
+  constructor(props) {
+    super(props);
+    this.handelLoginClick = this.handelLoginClick.bind(this);
+    this.handelLogoutClick = this.handelLogoutClick.bind(this);
+    this.state = { isLoggedIn: props.isLoggedIn };
+    // console.log(props.isLoggedIn);
+  }
+
+  handelLoginClick() {
+    this.setState({ isLoggedIn: true });
+  }
+
+  handelLogoutClick() {
+    this.setState({ isLoggedIn: false });
+  }
+
+  render() {
+    const isLoggedIn = this.state.isLoggedIn;
+    let button;
+    if (isLoggedIn) {
+      button = <LoginButton onClick={this.handelLogoutClick} />;
+    } else {
+      button = <LogoutButton onClick={this.handelLoginClick} />;
+    }
+    return (
+      <div className="App">
+        <Message isLoggedIn={isLoggedIn} />
+        {button}
+      </div>
+    );
+  }
+}
+
+export default Greeting;
--- src/index.js
+++ src/index.js
@@ -1,14 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
-import MailBox from "./components/App";
+import Greeting from "./components/App";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-
-const messages = ["React", "React 1", "React 2", "React 3", "React 4"];
-
 root.render(
   <div>
-    <MailBox unreadMessages={messages} />
+    <Greeting isLoggedIn={false} />
   </div>
 );
