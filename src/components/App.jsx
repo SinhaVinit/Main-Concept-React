a1c2dfaeed4bd7a073a1c213a8ba9dd3a7fcca34
SinhaVinit <vinit.8444935532@gmail.com>
2024-01-16 18:34:15 +0530

    Form 2 (Basic form structure) (Controlled Components)

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,15 +1,43 @@
 import "./App.css";
+import React from "react";
 
-function App(props) {
-  return (
-    <form>
-      <label>
-        Name:
-        <input type="text" name="name" />
-      </label>
-      <input type="submit" value="Submit" />
-    </form>
-  );
+class NameForm extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { value: "" };
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange(event) {
+    // console.log(event);
+    this.setState({ value: event.target.value });
+  }
+
+  handleSubmit(event) {
+    // console.log(event);
+    alert("A name was submitted: " + this.state.value);
+    // After submitting the form the page is refreshing. To prevent this we have to use preventDefault().
+    event.preventDefault();
+  }
+
+  render() {
+    return (
+      <form className="App" onSubmit={this.handleSubmit}>
+        <label>
+          Name:
+          <input
+            type="text"
+            placeholder="Enter your name"
+            value={this.state.value}
+            onChange={this.handleChange}
+          />
+        </label>
+        <input type="submit" value="Submit" />
+      </form>
+    );
+  }
 }
 
-export default App;
+export default NameForm;
