5ce9f52312300f0759f5278bd3bb1713b0c40d64
SinhaVinit <vinit.8444935532@gmail.com>
2024-01-16 18:07:17 +0530

    Forms 1

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,22 +1,14 @@
 import "./App.css";
 
-function ListItem(props) {
-  return (
-    <div className="App">
-      <h1>{props.value.title}</h1>
-      <p>{props.value.content}</p>
-    </div>
-  );
-}
-
 function App(props) {
-  const posts = props.posts;
   return (
-    <div>
-      {posts.map((post) => (
-        <ListItem key={post.id} value={post} />
-      ))}
-    </div>
+    <form>
+      <label>
+        Name:
+        <input type="text" name="name" />
+      </label>
+      <input type="submit" value="Submit" />
+    </form>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -3,14 +3,9 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./components/App";
 
-const posts = [
-  { id: 1, title: "Hello World", content: "Welcome to learning React!" },
-  { id: 2, title: "Installation", content: "You can install React from npm." },
-];
-
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <div>
-    <App posts={posts} />
+    <App />
   </div>
 );
