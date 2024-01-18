9c40f00c872637067df805926b434a37df190584
SinhaVinit <vinit.8444935532@gmail.com>
2024-01-18 16:08:49 +0530

    Form 8 (Uncontrolled Components) (Input Tag)

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,1 +1,38 @@
+// In most cases, we recommend using controlled components to implement forms.
+// In a controlled component, form data is handled by a React component.
+// The alternative is uncontrolled components, where form data is handled by the DOM itself.
+
+// To write an uncontrolled component, instead of writing an event handler for every state update,
+// you can use a ref to get form values from the DOM.
+
 import "./App.css";
+import React from "react";
+
+class NameForm extends React.Component {
+  constructor(props) {
+    super(props);
+    this.input = React.createRef();
+
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleSubmit(event) {
+    alert("Your name is: " + this.input.current.value);
+    event.preventDefault();
+  }
+
+  render() {
+    return (
+      <form onSubmit={this.handleSubmit}>
+        <label>
+          Enter Your Name:
+          <input type="text" ref={this.input} placeholder="Enter your name" />
+        </label>
+        <button type="submit" value="Submit">
+          Submit
+        </button>
+      </form>
+    );
+  }
+}
+export default NameForm;
--- src/index.js
+++ src/index.js
@@ -1,19 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
-import Reservation from "./components/App";
+import NameForm from "./components/App";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<input value="hi" />);
-
-setTimeout(function () {
-  const root = ReactDOM.createRoot(document.getElementById("root"));
-  root.render(<input value={null} />);
-}, 1000);
-
-// const root = ReactDOM.createRoot(document.getElementById("root"));
-// root.render(
-//   <div>
-//     <Reservation />
-//   </div>
-// );
+root.render(
+  <div>
+    <NameForm />
+  </div>
+);
