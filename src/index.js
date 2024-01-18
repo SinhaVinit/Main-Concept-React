d89b3daf0ed87cd962dcae1efb54f28903293146
SinhaVinit <vinit.8444935532@gmail.com>
2024-01-18 11:26:21 +0530

    Form 7 (Controlled Input Null Value)

--- src/index.js
+++ src/index.js
@@ -4,8 +4,16 @@ import "./index.css";
 import Reservation from "./components/App";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(
-  <div>
-    <Reservation />
-  </div>
-);
+root.render(<input value="hi" />);
+
+setTimeout(function () {
+  const root = ReactDOM.createRoot(document.getElementById("root"));
+  root.render(<input value={null} />);
+}, 1000);
+
+// const root = ReactDOM.createRoot(document.getElementById("root"));
+// root.render(
+//   <div>
+//     <Reservation />
+//   </div>
+// );
