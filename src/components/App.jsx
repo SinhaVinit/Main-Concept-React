f8f80628c9c391988644666567b8ce2f2bd6974e
SinhaVinit <vinit.8444935532@gmail.com>
2024-01-23 17:40:42 +0530

    Composition vs Inheritance 3 (Specialization) (WelcomeDialog)

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,15 +1,24 @@
-import SplitPane from "./SplitPane";
-
-function Contacts() {
-  return <div className="Contacts" />;
-}
-
-function Chat() {
-  return <div className="Chat" />;
-}
+import Dialog from "./Dialog";
 
 function App() {
-  return <SplitPane left={<Contacts />} right={<Chat />} />;
-}
+  const messages = [
+    { title: "Welcome", message: "Thank you for visiting our spacecraft!" },
+    { title: "नमस्ते", message: "हमारे अंतरिक्ष यान पर आने के लिए धन्यवाद" },
+    {
+      title: "いらっしゃいませ",
+      message: "私たちの宇宙船をご覧いただきありがとうございます",
+    },
+  ];
 
+  return (
+    <div>
+      {messages.map((message) => (
+        <div>
+          <Dialog title={message.title} message={message.message} />
+          <br />
+        </div>
+      ))}
+    </div>
+  );
+}
 export default App;
---
+++ src/components/Dialog.jsx
@@ -0,0 +1,18 @@
+function FancyBorder(props) {
+  return (
+    <div className={"FancyBorder FancyBorder-" + props.colour}>
+      {props.children}
+    </div>
+  );
+}
+
+function Dialog(props) {
+  return (
+    <FancyBorder colour="blue">
+      <h1>{props.title}</h1>
+      <p>{props.message}</p>
+    </FancyBorder>
+  );
+}
+
+export default Dialog;
