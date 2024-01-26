7fd0a1f4cd26070f6a51beb21a8563e4e0fe8911
SinhaVinit <vinit.8444935532@gmail.com>
2024-01-26 22:56:03 +0530

    Introducing Hooks 2 (using useEffect)

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,7 +1,13 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
 function App() {
   const [count, setCount] = useState(0);
+
+  useEffect(() => {
+    document.title = `You clicked ${count} times`;
+    console.log(document.title);
+  });
+
   return (
     <div>
       <p>You clicked {count} times.</p>
